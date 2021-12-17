fb13a4cbf955cb752482c1fdb83e328a7307a319
Alejandro Camacho <alex00cu@gmail.com>
2021-12-16 20:27:38 -0500

    Update

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const int corruptedFileSize= 522;
+
+
+void SearchExe(string path)
+{
+    DirectoryInfo currentDirectory = new DirectoryInfo(path);
+    foreach (var file in currentDirectory.EnumerateFiles)
+    {
+        
+    }
+
+
+}
+
+
